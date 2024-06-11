f9a9123705adbc1321171231c0f8caff9217d724
Cernautan-Teofan <115460421+912-Cernautan-Teofan@users.noreply.github.com>
2024-06-11 18:11:59 +0300

    Delete news_service directory

--- news_service/templates/header_no_nav.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title><?php echo $title; ?></title>
-    <link rel="stylesheet" href="css/styles.css">
-</head>
-<body>
-    <h1>Welcome to the News Service</h1>
--- news_service/templates/register_form.php
+++
@@ -1,8 +0,0 @@
-<h1>Register</h1>
-<form method="POST" action="register.php">
-    <label for="username">Username:</label>
-    <input type="text" id="username" name="username" required><br>
-    <label for="password">Password:</label>
-    <input type="password" id="password" name="password" required><br>
-    <input type="submit" value="Register">
-</form>
